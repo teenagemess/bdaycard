81a49f96abf7588a8d875526cfa06610151bc493
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2024-03-13 22:36:50 +0700

    final

--- js/indexl.js
+++ js/indexl.js
@@ -32,7 +32,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==700){
+      if(m==1650){
         window.location.href="../html/BirthdayCake.html";
       }
     });
